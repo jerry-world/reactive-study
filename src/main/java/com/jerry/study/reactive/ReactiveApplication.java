26750b4d4559116debe1d916a27a91149738351b
small-dogg <sy920112@naver.com>
2025-01-02 13:21:25 +0900

    커밋 미 반영분 적용(Reactor Flux 사용해보기)

--- src/main/java/com/jerry/study/reactive/ReactiveApplication.java
+++ src/main/java/com/jerry/study/reactive/ReactiveApplication.java
@@ -2,9 +2,38 @@ package com.jerry.study.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Publisher;
 
 @SpringBootApplication
 public class ReactiveApplication {
+
+    @RestController
+    public static class Controller {
+        @RequestMapping("/hello")
+        public Publisher<String> hello(String name) {
+            return new Publisher<String>() {
+                @Override
+                public void subscribe(Flow.Subscriber<? super String> subscriber) {
+                    subscriber.onSubscribe(new Flow.Subscription() {
+                        @Override
+                        public void request(long n) {
+                            subscriber.onNext("Hello " + name);
+                            subscriber.onComplete();
+                        }
+
+                        @Override
+                        public void cancel() {
+
+                        }
+                    });
+                }
+            };
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveApplication.class, args);
--- src/main/java/com/jerry/study/reactive/example/ReactorEx.java
+++ src/main/java/com/jerry/study/reactive/example/ReactorEx.java
@@ -1,2 +1,23 @@
-package com.jerry.study.reactive.example;public class ReactorEx {
+package com.jerry.study.reactive.example;
+
+import reactor.core.publisher.Flux;
+
+//리액터는 Publisher를 구현한 Flux를 제공
+public class ReactorEx {
+    public static void main(String[] args) {
+        Flux.<Integer>create(e -> {
+                    e.next(1);
+                    e.next(2);
+                    e.next(3);
+                    e.next(4);
+                    e.next(5);
+                    e.complete();
+                })
+                .log()
+                .map(s -> s * 10)
+                .log()
+                .reduce(0, (a, b) -> a + b)
+                .log()
+                .subscribe(System.out::println);
+    }
 }
