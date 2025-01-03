6600c4d48585245e3b7835394c7eb95fed666b62
small-dogg <sy920112@naver.com>
2025-01-03 13:32:01 +0900

    Future 맛보기

---
+++ src/main/java/com/jerry/study/reactive/async/FutureEx.java
@@ -0,0 +1,27 @@
+package com.jerry.study.reactive.async;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class FutureEx {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Future<String> f = es.submit(() -> {
+            Thread.sleep(2000);
+            log.info("Async");
+            return "Hello";
+        });
+
+
+        while(!f.isDone()) {
+            log.info("Exit");// submit 동작이 동작되는 중에 병렬적으로 또다른 작업을 할 수 있어.
+            log.info(f.get()); // Future.get() : Blocking
+        }
+    }
+}
