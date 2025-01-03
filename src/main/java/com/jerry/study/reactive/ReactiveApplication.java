9abcc8366e36aa64b33a2ce73260648451dab94a
small-dogg <sy920112@naver.com>
2025-01-03 16:17:27 +0900

    (Deprecated) ListenableFuture 인터페이스에 Async 애너테이션을 적용해서 비동기 만들어보기

--- src/main/java/com/jerry/study/reactive/ReactiveApplication.java
+++ src/main/java/com/jerry/study/reactive/ReactiveApplication.java
@@ -1,13 +1,28 @@
 package com.jerry.study.reactive;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Flow;
 import java.util.concurrent.Flow.Publisher;
 
+@Slf4j
+@RequiredArgsConstructor
+@EnableAsync
 @SpringBootApplication
 public class ReactiveApplication {
 
@@ -35,8 +50,51 @@ public class ReactiveApplication {
         }
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReactiveApplication.class, args);
+    @Service
+    @Slf4j
+    public static class MyService {
+
+        //        @Async //AOP 동작 구조
+//        public Future<String> hello() throws InterruptedException {
+//            Thread.sleep(1000);
+//            log.debug("hello()");
+//            return new AsyncResult<>("hello");
+//        }
+        @Async("threadPoolTaskExecutor")
+        public ListenableFuture<String> hello() throws InterruptedException {
+            Thread.sleep(1000);
+            log.debug("hello()");
+            return new AsyncResult<>("hello");
+        }
+
     }
 
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+        te.setCorePoolSize(10);
+        te.setMaxPoolSize(100);
+        te.setQueueCapacity(200);
+        te.setThreadNamePrefix("MyThread-");
+        te.initialize();
+        return te;
+    }
+
+    public static void main(String[] args) {
+        try (ConfigurableApplicationContext c = SpringApplication.run(ReactiveApplication.class, args)) {
+        }
+    }
+
+    private final MyService myService;
+
+    @Bean
+    ApplicationRunner run() {
+        return _ -> {
+            log.info("run()");
+            ListenableFuture<String> f = myService.hello();
+            f.addCallback(s -> System.out.println(s), e -> System.out.println(e));
+            log.info("exit : " + f.isDone());
+            log.info("result : " + f.get());
+        };
+    }
 }
