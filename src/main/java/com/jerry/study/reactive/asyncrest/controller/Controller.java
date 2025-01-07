2ac1f0b3de50be56d4c1d3cc6a61eaf93fe04deb
jerry-world <sy920112@naver.com>
2025-01-07 17:07:59 +0900

    CompletableFuture 찍먹2

--- src/main/java/com/jerry/study/reactive/asyncrest/controller/Controller.java
+++ src/main/java/com/jerry/study/reactive/asyncrest/controller/Controller.java
@@ -1,13 +1,14 @@
 package com.jerry.study.reactive.asyncrest.controller;
 
-import com.jerry.study.reactive.asyncrest.completion.Completion;
 import com.jerry.study.reactive.asyncrest.service.Service;
 import io.netty.channel.nio.NioEventLoopGroup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.CompletableFuture;
@@ -23,7 +24,7 @@ public class Controller {
     //SpringBoot 2.7.1
     //비동기 작업을 처리하기 위해서, Background에 스레드를 생성하서 동작함
     AsyncRestTemplate art = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
-//    RestTemplate restTemplate = new RestTemplate();
+    RestTemplate rt = new RestTemplate();
 
     @GetMapping("/rest")
     public DeferredResult<String> rest(int idx) throws ExecutionException, InterruptedException {
@@ -32,13 +33,23 @@ public class Controller {
 
         DeferredResult<String> dr = new DeferredResult<>();
 
+        //CompletableFuture로 감싸기
+        toCF(art.getForEntity("http://localhost:8081/remote?req={req}", String.class, "h" + idx))
+                .thenCompose(s -> toCF(art.getForEntity("http://localhost:8081/remote2?req={req}", String.class, s.getBody())))
+                .thenApplyAsync(s2 -> service.work(s2.getBody()))
+                .thenAccept(s3 -> dr.setResult(s3))
+                .exceptionally(e -> {
+                    dr.setErrorResult(e.getMessage());
+                    return null;
+                });
+
         //Callback Hell 탈출하기
-        Completion
-                .from(art.getForEntity("http://localhost:8081/remote?req={req}", String.class, "h" + idx))
-                .andApply(s -> art.getForEntity("http://localhost:8081/remote2?req={req}", String.class, s.getBody()))
-                .andApply(s -> service.work(s.getBody()))
-                .andError(e -> dr.setErrorResult(e.toString()))
-                .andAccept(dr::setResult); // Consumer Interface 를 구현한.
+//        Completion
+//                .from(art.getForEntity("http://localhost:8081/remote?req={req}", String.class, "h" + idx))
+//                .andApply(s -> art.getForEntity("http://localhost:8081/remote2?req={req}", String.class, s.getBody()))
+//                .andApply(s -> service.work(s.getBody()))
+//                .andError(e -> dr.setErrorResult(e.toString()))
+//                .andAccept(dr::setResult); // Consumer Interface 를 구현한.
 
 
 //        ListenableFuture<ResponseEntity<String>> f1 = art.getForEntity("http://localhost:8081/remote?req={req}", String.class, "hello " + idx);
@@ -60,5 +71,14 @@ public class Controller {
 //                e1 -> dr.setErrorResult(e1.getMessage())
 //        );
         return dr;
+    }
+
+    <T> CompletableFuture<T> toCF(ListenableFuture<T> lf) {
+        CompletableFuture<T> cf = new CompletableFuture<>();
+        lf.addCallback(
+                cf::complete,
+                cf::completeExceptionally
+        );
+        return cf;
     }
 }
--- src/main/java/com/jerry/study/reactive/asyncrest/service/Service.java
+++ src/main/java/com/jerry/study/reactive/asyncrest/service/Service.java
@@ -1,14 +1,12 @@
 package com.jerry.study.reactive.asyncrest.service;
 
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
-import org.springframework.util.concurrent.ListenableFuture;
 
 @org.springframework.stereotype.Service
 public class Service {
 
     @Async
-    public ListenableFuture<String> work(String req) {
-        return new AsyncResult<>(req + "/asyncwork");
+    public String work(String req) {
+        return req + "/asyncwork";
     }
 }
